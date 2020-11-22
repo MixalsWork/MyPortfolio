fca36dc9aa1978ad2e4cd7b2f3e0073525f28049
MixalsWork <Mixals.work@gmail.com>
2020-11-22 17:57:04 +0200

    new

---
+++ projects/glassshop/js/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        
+        
+        speed: 600,
+       
+      });
+          
+  });
+$(document).ready(function(){
+    $('.products__mini-slider').slick({
+    slideToSHow: 1,
+    dots: true,
+    arrows: false,
+    speed: 600,
+    
+    });
+        
+});
+ 
